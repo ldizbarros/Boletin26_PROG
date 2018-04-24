8b6353cfa7788ba18cded295911ab885ce47823b
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-04-24 08:46:00 +0200

    Boletin 26 de Programacion

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,10 @@
+package boletin26;
+
+public class Boletin26 {
+
+    public static void main(String[] args) {
+        Ventana paginaPrincipal = new Ventana();
+        paginaPrincipal.setVisible(true);
+    }
+    
+}
